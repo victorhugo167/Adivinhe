c30e52d40da721d944d468f05cdd6b25dcebed42
victorhugo167 <00001092308088sp@al.educacao.sp.gov.br>
2024-11-19 16:46:59 -0300

    colocando projeto adivinhe no github

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'jogos',
+    'Qual é o jogo em que você precisa correr de um guarda, entre trilhos de trem?',
+    'O jogo é SubWay Suffers'
+)
+
+criaCartao(
+    'animais',
+    'Qual o maior animal do mundo?',
+    'O maior animal do mundo é a baleia azul, que pode medir até 30 metros'
+)
+
+criaCartao(
+    'planetas',
+    'Qual é o menor planeta do sistema solar?',
+    'O menor planeta é Mercúrio'
+)
+
+criaCartao(
+    'conehcimentos gerais',
+    'Quem inventou a  lâmpada?',
+    'Foi Thomas Edison, porém ele aperfeiçoou a ideia de outros inventores'
+)
